b5ca8c117f0200cd66422d1a1555c9f68a2e0432
devin <yuoh0109@gmail.com>
2025-01-24 20:55:29 -0700

    test1

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About: React.FC = () => {
   return (
     <div>
-      <h1>About Us</h1>
+      <h1>호호호About Us</h1>
       <p>We are a tech company providing web solutions.</p>
     </div>
   );
