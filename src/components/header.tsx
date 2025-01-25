b07d26302a133b3c6e928f93a9e1a3bedde84f47
Young <yuoh0109@gmail.com>
2025-01-24 22:27:05 -0700

    test3

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,24 +5,27 @@ import { Link } from "react-router-dom";
 import "./header.css";
 
 const Header: React.FC = () => {
-    return( 
-        <header className="header">
-            <nav className="navbar">
-                <ul className="menu">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/portfolio">Portfolio</Link>
-                    </li>
-                    <li> 
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul> 
-            </nav>
-        <header/>
-    );
+	return (
+		<header className='header'>
+			<nav className='navbar'>
+				<ul className='menu'>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+					<li>
+						<Link to='/portfolio'>Portfolio</Link>
+					</li>
+					<li>
+						<Link to='/contact'>Contact</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
+
+
+export default Header;
