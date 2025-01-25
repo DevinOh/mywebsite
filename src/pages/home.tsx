9ca4650e4912f797f1f31fdc9e416ee1687f9d48
Young <yuoh0109@gmail.com>
2025-01-24 20:57:11 -0700

    test2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home: React.FC = () => {
 	return (
 		<div>
-			<h1>안녕하세여Welcome to Our Website</h1>
+			<h1>낄낄낄안녕하세여Welcome to Our Website</h1>
 			<p>Discover more about us!</p>
 		</div>
 	);
